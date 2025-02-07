b7da9988f8bc35a64840efdf65dd103177fcc170
Burrito-Princess <113419432+Burrito-Princess@users.noreply.github.com>
2025-02-07 19:33:58 +0100

    in proper trees gone

--- main.js
+++ main.js
@@ -34,38 +34,22 @@ let perlin = {
     let xf = Math.floor(x);
     let yf = Math.floor(y);
 
-    let xf_t = Math.floor(x);
-    let yf_t = Math.floor(y);
-
     //define the corners
     let tl = this.dot_prod_grid(x, y, xf, yf).vect;
     let tr = this.dot_prod_grid(x, y, xf + 1, yf).vect;
     let bl = this.dot_prod_grid(x, y, xf, yf + 1).vect;
     let br = this.dot_prod_grid(x, y, xf + 1, yf + 1).vect;
-
-    let tl_t = this.dot_prod_grid(x, y, xf, yf).t;
-    let tr_t = this.dot_prod_grid(x, y, xf + 1, yf).t;
-    let bl_t = this.dot_prod_grid(x, y, xf, yf + 1).t;
-    let br_t = this.dot_prod_grid(x, y, xf + 1, yf + 1).t;
     //
     let xt = this.interp(x - xf, tl, tr);
     let xb = this.interp(x - xf, bl, br);
 
-    let xt_t = this.interp(x - xf_t, tl_t, tr_t);
-    let xb_t = this.interp(x - xf_t, bl_t, br_t);
-
     let v = this.interp(y - yf, xt, xb);
-    let t = this.interp(y - yf_t, xt_t, xb_t);
 
     // debugger;
     // console.log(t);
     
     
     v = Math.floor(v*7);
-    if ((v==0) && t == 1){
-      // console.log("TREE")
-      return 0.5;
-    }
     return v; 
 
   },
